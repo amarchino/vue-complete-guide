085a8d93236caabbd53f88d8b98fdd3aec3d3086
amarchino <marchino.alessandro@gmail.com>
2021-04-15 21:47:02 +0200

    Lesson 034

--- section-02/06-styling/app.js
+++ section-02/06-styling/app.js
@@ -6,6 +6,17 @@ Vue.createApp({
       boxCSelected: false,
     };
   },
+  computed: {
+    boxAClasses() {
+      return {active: this.boxASelected};
+    },
+    boxBClasses() {
+      return {active: this.boxBSelected};
+    },
+    boxCClasses() {
+      return {active: this.boxCSelected};
+    }
+  },
   methods: {
     boxSelected(box) {
       const selection = `box${box}Selected`
