d5cd948a46fb4ad432df54a33a3d215a9b8fd3e9
amarchino <marchino.alessandro@gmail.com>
2021-04-17 12:38:01 +0200

    Lesson 040

---
+++ section-03/01-lists/app.js
@@ -0,0 +1,16 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      enteredGoalValue: '',
+      goals: []
+    };
+  },
+  methods: {
+    addGoals() {
+      this.goals.push(this.enteredGoalValue);
+      this.enteredGoalValue = '';
+    }
+  }
+});
+
+app.mount('#user-goals');
