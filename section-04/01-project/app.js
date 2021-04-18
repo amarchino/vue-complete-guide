09c7f31548c61234c9fce0fdcb1e5d55b918611d
amarchino <marchino.alessandro@gmail.com>
2021-04-18 20:39:55 +0200

    Lesson 052

--- section-04/01-project/app.js
+++ section-04/01-project/app.js
@@ -6,16 +6,34 @@ const app = Vue.createApp({
   data() {
     return {
       playerHealth: 100,
-      monsterHealth: 100
+      monsterHealth: 100,
+      currentRound: 0
     };
+  },
+  computed: {
+    monsterBarStyles() {
+      return { width: this.monsterHealth + '%' };
+    },
+    playerBarStyles() {
+      return { width: this.playerHealth + '%' };
+    },
+    mayUseSpecialAttack() {
+      return this.currentRound % 3 !== 0;
+    }
   },
   methods: {
     attackMonster() {
+      this.currentRound++;
       this.monsterHealth -= getRandomValue(5, 12);
       this.attackPlayer();
     },
     attackPlayer() {
-      this.playerHealth -= getRandomValue(5, 12);
+      this.playerHealth -= getRandomValue(8, 15);
+    },
+    specialAttackMonster() {
+      this.currentRound++;
+      this.monsterHealth -= getRandomValue(10, 25);
+      this.attackPlayer();
     }
   }
 });
