7685ed4cb91f4cd323b2d7061fc35a3e5a0df3f3
amarchino <marchino.alessandro@gmail.com>
2021-04-22 22:15:21 +0200

    Lesson 084

--- section-07/02-vue-project/src/main.js
+++ section-07/02-vue-project/src/main.js
@@ -1,4 +1,8 @@
 import { createApp } from 'vue';
-import App from './App';
 
-createApp(App).mount('#app');
+import App from './App';
+import FriendContact from './components/FriendContact';
+
+const app = createApp(App);
+app.component('friend-contact', FriendContact);
+app.mount('#app');
