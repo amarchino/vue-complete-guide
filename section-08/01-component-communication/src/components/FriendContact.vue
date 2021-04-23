b52aeaf2466156674e2a2c0addf2b275a6abc8c7
amarchino <marchino.alessandro@gmail.com>
2021-04-23 22:52:56 +0200

    Lesson 089

---
+++ section-08/01-component-communication/src/components/FriendContact.vue
@@ -0,0 +1,42 @@
+<template>
+  <li>
+    <h2>{{ name }}</h2>
+    <button @click="toggleDetails">{{ detailsAreVisible ? 'Hide' : 'Show' }} Details</button>
+    <ul v-if="detailsAreVisible">
+      <li>
+        <strong>Phone:</strong>
+        {{ phoneNumber }}
+      </li>
+      <li>
+        <strong>Email:</strong>
+        {{ emailAddress }}
+      </li>
+    </ul>
+  </li>
+</template>
+
+<script>
+export default {
+  props: [
+    'name',
+    'phoneNumber',
+    'emailAddress'
+  ],
+  data() {
+    return {
+      detailsAreVisible: false,
+      // friend: {
+      //   id: "manuel",
+      //   name: this.name,
+      //   phone: this.phoneNumber,
+      //   email: this.emailAddress,
+      // },
+    };
+  },
+  methods: {
+    toggleDetails() {
+      this.detailsAreVisible = !this.detailsAreVisible;
+    }
+  }
+};
+</script>
