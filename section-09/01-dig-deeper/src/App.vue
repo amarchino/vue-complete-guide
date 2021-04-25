30861d5df9ecfd08fd5ff2b81770f56c4915d52a
amarchino <marchino.alessandro@gmail.com>
2021-04-25 21:06:05 +0200

    Lesson 113

--- section-09/01-dig-deeper/src/App.vue
+++ section-09/01-dig-deeper/src/App.vue
@@ -7,11 +7,16 @@
       :info-text="activeUser.description"
       :role="activeUser.role"
     ></user-info>
+    <course-goals #default="slotProps">
+      <h2>{{ slotProps.item }}</h2>
+      <p>{{ slotProps.anotherProp }}</p>
+    </course-goals>
   </div>
 </template>
 
 <script>
 import BadgeList from './components/BadgeList.vue';
+import CourseGoals from './components/CourseGoals.vue';
 import TheHeader from './components/TheHeader.vue';
 import UserInfo from './components/UserInfo.vue';
 
@@ -20,7 +25,8 @@ export default {
     // 'the-header': TheHeader
     TheHeader,
     BadgeList,
-    UserInfo
+    UserInfo,
+    CourseGoals
   },
   data() {
     return {
