63caa364a0dce93d1626e76f8fd58467cf8da834
amarchino <marchino.alessandro@gmail.com>
2021-04-28 22:35:39 +0200

    Lesson 127

--- section-10/01-project/src/components/learning-resources/LearningResource.vue
+++ section-10/01-project/src/components/learning-resources/LearningResource.vue
@@ -3,7 +3,7 @@
     <base-card>
       <header>
         <h3>{{ title }}</h3>
-        <button>Delete</button>
+        <base-button mode="flat">Delete</base-button>
       </header>
       <p>{{ description }}</p>
       <nav>
@@ -30,7 +30,7 @@ header {
   justify-content: space-between;
   align-items: center;
 }
-
+``
 h3 {
   font-size: 1.25rem;
   margin: 0.5rem 0;
--- section-10/01-project/src/main.js
+++ section-10/01-project/src/main.js
@@ -2,7 +2,9 @@ import { createApp } from 'vue';
 
 import App from './App.vue';
 import BaseCard from './components/UI/BaseCard.vue';
+import BaseButton from './components/UI/BaseButton.vue';
 
 createApp(App)
 .component('base-card', BaseCard)
+.component('base-button', BaseButton)
 .mount('#app');
