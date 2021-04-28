c049a77ce825621d53838204aa6f6b7fbe0bb57f
amarchino <marchino.alessandro@gmail.com>
2021-04-28 22:19:20 +0200

    Lesson 125

--- section-10/01-project/src/components/learning-resources/LearningResource.vue
+++ section-10/01-project/src/components/learning-resources/LearningResource.vue
@@ -3,7 +3,7 @@
     <div>
       <header>
         <h3>{{ title }}</h3>
-        <nutton>Delete</nutton>
+        <button>Delete</button>
       </header>
     </div>
     <p>{{ description }}</p>
@@ -19,6 +19,34 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+li {
+  margin: auto;
+  max-width: 40rem;
+}
 
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+h3 {
+  font-size: 1.25rem;
+  margin: 0.5rem 0;
+}
+
+p {
+  margin: 0.5rem 0;
+}
+
+a {
+  text-decoration: none;
+  color: #ce5c00;
+}
+
+a:hover,
+a:active {
+  color: #c89300;
+}
 </style>
