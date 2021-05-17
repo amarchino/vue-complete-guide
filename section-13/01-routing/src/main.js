356c937e7fb0b67a5b380f9ee483718ddf75d353
amarchino <marchino.alessandro@gmail.com>
2021-05-17 23:07:00 +0200

    Lesson 165

--- section-13/01-routing/src/main.js
+++ section-13/01-routing/src/main.js
@@ -1,14 +1,20 @@
 import { createApp } from 'vue';
 import { createRouter, createWebHistory } from 'vue-router';
 
+import TeamsList from './components/teams/TeamsList.vue';
+import UsersList from './components/users/UsersList.vue';
+
 import App from './App.vue';
 
 const router = createRouter({
-  routes: [],
-  history: createWebHistory()
+  history: createWebHistory(),
+  routes: [
+    { path: '/teams', component: TeamsList },
+    { path: '/users', component: UsersList },
+  ],
 });
 
 const app = createApp(App);
-
+app.use(router);
 
 app.mount('#app');
