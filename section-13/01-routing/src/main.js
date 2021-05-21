408f6276ef52438ec29039516441bb90710647fc
amarchino <marchino.alessandro@gmail.com>
2021-05-21 23:47:53 +0200

    Lesson 178

--- section-13/01-routing/src/main.js
+++ section-13/01-routing/src/main.js
@@ -3,7 +3,9 @@ import { createRouter, createWebHistory } from 'vue-router';
 
 import TeamsList from './components/teams/TeamsList.vue';
 import TeamMembers from './components/teams/TeamMembers.vue';
+import TeamsFooter from './components/teams/TeamsFooter.vue';
 import UsersList from './components/users/UsersList.vue';
+import UsersFooter from './components/users/UsersFooter.vue';
 import NotFound from './components/nav/NotFound.vue';
 
 import App from './App.vue';
@@ -12,10 +14,29 @@ const router = createRouter({
   history: createWebHistory(),
   routes: [
     { path: '/', redirect: '/teams' },
-    { name: 'teams', path: '/teams', component: TeamsList, children: [
-      { name: 'team-members', path: ':teamId', component: TeamMembers, props: true },
-    ] },
-    { path: '/users', component: UsersList },
+    {
+      name: 'teams',
+      path: '/teams',
+      components: {
+        default: TeamsList,
+        footer: TeamsFooter
+      },
+      children: [
+        {
+          name: 'team-members',
+          path: ':teamId',
+          component: TeamMembers,
+          props: true
+        }
+      ]
+    },
+    {
+      path: '/users',
+      components: {
+        default: UsersList,
+        footer: UsersFooter
+      }
+    },
     { path: '/:notFound(.*)', component: NotFound }
   ],
   linkActiveClass: 'active'
