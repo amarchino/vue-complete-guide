5b0044baebd76a6d8baca4699e49ee2ca2f7e0de
amarchino <marchino.alessandro@gmail.com>
2021-05-21 23:41:19 +0200

    Lesson 177

--- section-13/01-routing/src/components/teams/TeamsItem.vue
+++ section-13/01-routing/src/components/teams/TeamsItem.vue
@@ -11,8 +11,7 @@ export default {
   props: [ 'id', 'name', 'memberCount' ],
   computed: {
     teamMembersLink() {
-      // return '/teams/' + this.id;
-      return { name: 'team-members', params: { teamId: this.id } };
+      return { name: 'team-members', params: { teamId: this.id }, query: { sort: 'asc' } };
     }
   }
 };
