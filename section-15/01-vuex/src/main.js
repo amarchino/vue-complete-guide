bc267c3dadd63934cc4a5db4989007b8fa495f36
amarchino <marchino.alessandro@gmail.com>
2021-05-24 23:55:57 +0200

    Lesson 214

--- section-15/01-vuex/src/main.js
+++ section-15/01-vuex/src/main.js
@@ -31,6 +31,14 @@ const store = createStore({
       }
       return finalCounter;
     }
+  },
+  actions: {
+    increment(context) {
+      setTimeout(() => context.commit('increment'), 2000);
+    },
+    increase(context, payload) {
+      context.commit('increase', payload);
+    }
   }
 });
 
