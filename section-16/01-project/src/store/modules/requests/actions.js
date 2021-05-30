539db62d29d9808b95465f02eccfcc5405dd5409
amarchino <marchino.alessandro@gmail.com>
2021-05-30 22:45:29 +0200

    Lesson 252

--- section-16/01-project/src/store/modules/requests/actions.js
+++ section-16/01-project/src/store/modules/requests/actions.js
@@ -1,11 +1,39 @@
 export default {
-  contactCoach(context, payload) {
+  async contactCoach(context, payload) {
     const newRequest = {
-      id: new Date().toISOString(),
-      coachId: payload.coachId,
       userEmail: payload.email,
       message: payload.message
     };
-    context.commit('addRequest', newRequest);
+    const response = await fetch(`https://vue-http-demo-f008d-default-rtdb.firebaseio.com/requests/${payload.coachId}.json`, {
+      method: 'POST',
+      body: JSON.stringify(newRequest)
+    });
+    const responseData = await response.json();
+    if(!response.ok) {
+      const error = new Error(responseData.message || 'Failed to send request.');
+      throw error;
+    }
+
+    context.commit('addRequest', {
+      ...newRequest,
+      coachId: payload.coachId,
+      id: responseData.name
+    });
+  },
+  async fetchRequests(context) {
+    const coachId = context.rootGetters.userId;
+    const response = await fetch(`https://vue-http-demo-f008d-default-rtdb.firebaseio.com/requests/${coachId}.json`);
+    const responseData = await response.json();
+    if(!response.ok) {
+      const error = new Error(responseData.message || 'Failed to send request.');
+      throw error;
+    }
+    const requests = Object.entries(responseData || [])
+    .map(([id, data]) => ({
+      ...data,
+      id,
+      coachId
+    }));
+    context.commit('setRequests', requests);
   }
 };
