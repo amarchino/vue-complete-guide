643d9b6e894ee8519cea6826ebb3fa3dc472bb85
amarchino <marchino.alessandro@gmail.com>
2021-06-06 21:58:30 +0200

    Lesson 264

--- section-17/01-auth/src/store/modules/auth/actions.js
+++ section-17/01-auth/src/store/modules/auth/actions.js
@@ -1,5 +1,25 @@
 export default {
-  login() {},
+  async login(context, payload) {
+    const response = await fetch('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyCc9wS3w8ryAvl0VV4hVHyjNWVedxT0y1o', {
+      method: 'POST',
+      body: JSON.stringify({
+        email: payload.email,
+        password: payload.password,
+        returnSecureToken: true
+      })
+    });
+    const responseData = await response.json();
+
+    if (!response.ok) {
+      throw new Error(responseData.message || 'Failed to authenticate. Check your login data');
+    }
+
+    context.commit('setUser', {
+      token: responseData.idToken,
+      userId: responseData.localId,
+      tokenExpiration: responseData.expiresIn
+    });
+  },
   async signup(context, payload) {
     const response = await fetch('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCc9wS3w8ryAvl0VV4hVHyjNWVedxT0y1o', {
       method: 'POST',
