817eddcc75e357b7e24c0d76f7959a955c23f495
amarchino <marchino.alessandro@gmail.com>
2021-06-06 22:03:41 +0200

    Lesson 265

--- section-17/01-auth/src/store/modules/coaches/actions.js
+++ section-17/01-auth/src/store/modules/coaches/actions.js
@@ -8,7 +8,8 @@ export default {
       hourlyRate: data.rate,
       areas: data.areas
     };
-    const response = await fetch(`https://vue-http-demo-f008d-default-rtdb.firebaseio.com/coaches/${userId}.json`, {
+    const token = context.rootGetters.token;
+    const response = await fetch(`https://vue-http-demo-f008d-default-rtdb.firebaseio.com/coaches/${userId}.json?auth=${token}`, {
       method: 'PUT',
       body: JSON.stringify(coachData)
     });
