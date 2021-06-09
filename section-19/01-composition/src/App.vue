6c6cfa5d21fb244ebd3b21717cb8388c3415bbd0
amarchino <marchino.alessandro@gmail.com>
2021-06-09 22:39:01 +0200

    Lesson 287

--- section-19/01-composition/src/App.vue
+++ section-19/01-composition/src/App.vue
@@ -4,8 +4,8 @@
     <h3>{{ age }}</h3>
     <button @click="setAge">Change Age</button>
     <div>
-      <input type="text" placeholder="First Name" @input="setFirstName"/>
-      <input type="text" placeholder="Last Name" @input="setLastName" />
+      <input type="text" placeholder="First Name" v-model="firstName" />
+      <input type="text" placeholder="Last Name" v-model="lastName" />
     </div>
   </section>
 </template>
@@ -17,18 +17,10 @@ export default {
     const firstName = ref('');
     const lastName = ref('');
     const uAge = ref(31);
-    const userName = computed(() => {
-      return firstName.value + ' ' + lastName.value;
-    })
+    const userName = computed(() => `${firstName.value} ${lastName.value}`);
 
     function setNewAge() {
       uAge.value = 32;
-    }
-    function setFirstName(event) {
-      firstName.value = event.target.value;
-    }
-    function setLastName(event) {
-      lastName.value = event.target.value;
     }
 
     return {
@@ -36,9 +28,7 @@ export default {
       firstName,
       lastName,
       age: uAge,
-      setAge: setNewAge,
-      setFirstName,
-      setLastName
+      setAge: setNewAge
     };
   }
 };
