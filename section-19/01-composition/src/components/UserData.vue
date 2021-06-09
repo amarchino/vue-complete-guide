39fe2b17c51a85f6ee6092c639c2087bc1dacba0
amarchino <marchino.alessandro@gmail.com>
2021-06-09 23:49:34 +0200

    Lesson 294

--- section-19/01-composition/src/components/UserData.vue
+++ section-19/01-composition/src/components/UserData.vue
@@ -4,13 +4,21 @@
 </template>
 
 <script>
-import { computed, inject } from '@vue/runtime-core';
+import { computed, inject, onBeforeMount, onBeforeUnmount, onBeforeUpdate, onMounted, onUnmounted, onUpdated } from '@vue/runtime-core';
 export default {
   props: ['firstName', 'lastName'],
   setup(props, context) {
     const age = inject('userAge');
     const uName = computed(() => `${props.firstName} ${props.lastName}`);
     console.log(context);
+
+    onBeforeMount(() => console.log('onBeforeMount'));
+    onMounted(() => console.log('onMounted'));
+    onBeforeUpdate(() => console.log('onBeforeUpdate'));
+    onUpdated(() => console.log('onUpdated'));
+    onBeforeUnmount(() => console.log('onBeforeUnmount'));
+    onUnmounted(() => console.log('onUnmounted'));
+
     return {
       userName: uName,
       age
