cfa3f89c41409395502527ca337365b577a7a95e
amarchino <marchino.alessandro@gmail.com>
2021-06-12 14:03:16 +0200

    Lesson 314

--- section-20/03-hooks/src/components/users/UserList.vue
+++ section-20/03-hooks/src/components/users/UserList.vue
@@ -19,7 +19,7 @@
 </template>
 
 <script>
-import { ref, computed } from 'vue';
+import { ref, computed, toRefs } from 'vue';
 import useSearch from '../../hooks/search';
 
 import UserItem from './UserItem.vue';
@@ -31,7 +31,8 @@ export default {
   props: ['users'],
   emits: ['list-projects'],
   setup(props) {
-    const { enteredSearchTerm, availableItems, updateSearch } = useSearch(props.users, 'fullName');
+    const { users } = toRefs(props);
+    const { enteredSearchTerm, availableItems, updateSearch } = useSearch(users, 'fullName');
 
     const sorting = ref(null);
     const displayedUsers = computed(function () {
