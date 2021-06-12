01a929036938f00f1b2217cfd5d827a35ea8e7c3
amarchino <marchino.alessandro@gmail.com>
2021-06-12 14:31:13 +0200

    Lesson 316

--- section-20/03-hooks/src/components/users/UserList.vue
+++ section-20/03-hooks/src/components/users/UserList.vue
@@ -19,8 +19,9 @@
 </template>
 
 <script>
-import { ref, computed, toRefs } from 'vue';
+import { toRefs } from 'vue';
 import useSearch from '../../hooks/search';
+import useSort from '../../hooks/sort';
 
 import UserItem from './UserItem.vue';
 
@@ -33,33 +34,12 @@ export default {
   setup(props) {
     const { users } = toRefs(props);
     const { enteredSearchTerm, availableItems, updateSearch } = useSearch(users, 'fullName');
-
-    const sorting = ref(null);
-    const displayedUsers = computed(function () {
-      if (!sorting.value) {
-        return availableItems.value;
-      }
-      return availableItems.value.slice().sort((u1, u2) => {
-        if (sorting.value === 'asc' && u1.fullName > u2.fullName) {
-          return 1;
-        } else if (sorting.value === 'asc') {
-          return -1;
-        } else if (sorting.value === 'desc' && u1.fullName > u2.fullName) {
-          return -1;
-        } else {
-          return 1;
-        }
-      });
-    });
-
-    function sort(mode) {
-      sorting.value = mode;
-    }
+    const { sorting, displayedItems, sort } = useSort(availableItems, 'fullName');
 
     return {
       enteredSearchTerm,
       updateSearch,
-      displayedUsers,
+      displayedUsers: displayedItems,
       sorting,
       sort
     };
